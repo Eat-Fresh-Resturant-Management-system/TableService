2f2dacd42c4371e84a98963f7e1785c7d210169f
MHA <201910895@post.au.dk>
2024-06-02 01:59:05 +0200

    mongo env

--- src/DB/mongodb.ts
+++ src/DB/mongodb.ts
@@ -1,15 +1,20 @@
-// Purpose: Connect to MongoDB
 import mongoose from 'mongoose';
 
-
-
 export async function connectToDatabase() {
-    try {
-      await mongoose.connect('mongodb://root:example@mongo-service:27017/', { dbName: "TableBooking" });
-      console.log("Connected to MongoDB");
+  const dbUser = process.env.MONGODB_USERNAME || 'root';
+  const dbPass = process.env.MONGODB_PASSWORD || 'example';
+  const dbHost = process.env.MONGODB_HOSTNAME || 'mongo-service';
+  const dbName = process.env.MONGODB_DBNAME || 'TableBooking';
   
-    //   await startServer();
-    } catch (error) {
-      console.error("Error connecting to MongoDB:", error);
-    }
+  const connectionString = `mongodb://${dbUser}:${dbPass}@${dbHost}:27017/${dbName}?authSource=admin`;
+
+  try {
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
   }
+}
