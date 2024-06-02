990164b693f8c35ffc9630567ba6ac9aaec04ceb
MHA <201910895@post.au.dk>
2024-06-02 02:16:30 +0200

    Update mongodb.ts

--- src/DB/mongodb.ts
+++ src/DB/mongodb.ts
@@ -6,13 +6,10 @@ export async function connectToDatabase() {
   const dbHost = process.env.MONGODB_HOSTNAME || 'mongo-service';
   const dbName = process.env.MONGODB_DBNAME || 'TableBooking';
   
-  const connectionString = `mongodb://${dbUser}:${dbPass}@${dbHost}:27017/${dbName}?authSource=admin`;
+  const connectionString = `mongodb://${dbUser}:${dbPass}@${dbHost}:27017`;
 
   try {
-    await mongoose.connect(connectionString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(connectionString, { dbName: "TableBooking" });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
